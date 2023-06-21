916898cc0fb5f6f0ba000697d14743c657355fd2
Sylum <106944410+ysakkaan20@users.noreply.github.com>
2023-06-22 00:42:51 +0300

    pucherzzzzzz

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,14 @@
 #include "main.h"
-
 /**
  * main - prints _putchar as a message
  *
  * Description: prints _putchar  using putchar prototype
  *
- * Return: 0 (when Success)
+ * Return: 0 (When Success)
  */
-
 int main(void)
 {
-	char text[10] = "_putchar";
+	char text[8] = "_putchar";
 	int i = 0;
 
 	for (i = 0; i < 9; i++)
